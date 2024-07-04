ccb8c291b7cef01004cd49f4b561d7c1e8d38d6a
vitorYghor <devictoryghor@gmail.com>
2024-07-04 19:24:46 -0300

    adding the corect dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.2" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:8.2.2")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+
+        classpath ("com.vanniktech:gradle-maven-publish-plugin:0.24.0")
+        classpath ("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+
+
+}
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
 }
