26ec2a2dd03264646db914a708564a26f85ee9b3
vitorYghor <devictoryghor@gmail.com>
2024-07-04 20:31:50 -0300

    teste com jetpackcompose

--- app/src/main/java/com/iara/MainActivity.kt
+++ app/src/main/java/com/iara/MainActivity.kt
@@ -1,15 +1,37 @@
-package com.iara;
+package com.iara
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.util.*;
-import android.os.Bundle;
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.iara.databinding.ActivityMainBinding
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = b.root
+        setContentView(view)
+        b.composeView.apply {
+            setViewCompositionStrategy((ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed))
+            setContent {
+                Text(
+                    text = "Texto branco com fundo preto",
+                    style = TextStyle(color = Color.White, fontSize = 18.sp)
+                )
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Test.doSomething();
+            }
+        }
     }
 }
