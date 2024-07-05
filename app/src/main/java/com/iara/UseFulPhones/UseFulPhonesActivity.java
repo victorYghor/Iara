581a55cbd865c962bb4e8e503a561a62cad96dad
heitorleonny <heitorleonnyyt1234@gmail.com>
2024-07-05 12:17:44 -0300

    add UseFulPhones logic

--- app/src/main/java/com/iara/UseFulPhones/UseFulPhonesActivity.java
+++ app/src/main/java/com/iara/UseFulPhones/UseFulPhonesActivity.java
@@ -1,6 +1,10 @@
 package com.iara.UseFulPhones;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +16,46 @@ import com.iara.R;
 
 public class UseFulPhonesActivity extends AppCompatActivity {
 
+    private Button defesaCivilButton;
+    private Button bombeirosButton;
+    private Button policiaMilitarButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_use_ful_phones);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button defesaCivilButton = findViewById(R.id.defesaCivilButton);
+        Button bombeirosButton = findViewById(R.id.bombeirosButton);
+        Button policiaMilitarButton = findViewById(R.id.policiaMilitarButton);
+
+        defesaCivilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber("199");
+            }
         });
+
+        bombeirosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber("193");
+            }
+        });
+
+        policiaMilitarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber("190");
+            }
+        });
+    }
+
+    private void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
