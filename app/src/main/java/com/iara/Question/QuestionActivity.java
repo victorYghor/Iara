05088cbe93c50c04b78ed729f7bb9517b1afae6b
heitorleonny <heitorleonnyyt1234@gmail.com>
2024-07-05 08:45:34 -0300

    add layouts

---
+++ app/src/main/java/com/iara/Question/QuestionActivity.java
@@ -0,0 +1,18 @@
+package com.iara.Question;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+
+        TextView answerTextView = findViewById(R.id.answer_text_view);
+        String answer = getIntent().getStringExtra("ANSWER");
+        answerTextView.setText(answer);
+    }
+}
